5f19be070415e4bc7ba462ec22cce763afc78fc6
CCHS RoboChargers <cchsrobochargers@gmail.com>
2019-11-05 19:33:31 -0500

    Added so much we dont know
    -- NGL

--- Testing-prgm-2019-09-25T00-46-38/Competition-2019-10-10T23-42-05/src/main.cpp
+++ Testing-prgm-2019-09-25T00-46-38/Competition-2019-10-10T23-42-05/src/main.cpp
@@ -16,10 +16,8 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
-#include "vex.h"
 #include "field-250.h"
-
-using namespace vex;
+#include "vex.h"
 
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
@@ -40,17 +38,29 @@ using namespace vex;
 // define your global instances of motors and other devices here
 vex::motor LeftMotor = vex::motor(vex::PORT3);
 vex::motor RightMotor = vex::motor(vex::PORT18);
-vex::motor LiftMotorL = vex::motor(vex::PORT20);
-vex::motor LiftMotorR = vex::motor(vex::PORT1);
-vex::motor Pinch1 = vex::motor(vex::PORT2);
-vex::motor Pinch2 = vex::motor(vex::PORT19);
+vex::motor LiftMotorL1 = vex::motor(vex::PORT20);
+vex::motor LiftMotorR1 = vex::motor(vex::PORT1);
+vex::motor LiftMotorL2 = vex::motor(vex::PORT19);
+vex::motor LiftMotorR2 = vex::motor(vex::PORT2);
+vex::motor Feed1 = vex::motor(vex::PORT4);
+vex::motor Feed2 = vex::motor(vex::PORT17);
 vex::competition Competition;
 
 int drivespeed = 75;
-int liftspeed = 100;
+int liftspeed = 65;
 double pi = 3.1415926535897;
 int autonspeed = 20;
 int pinchspeed = 50;
+int feedspeed = 100;
+
+void pulsedown() {
+  Feed1.rotateFor(800, vex::rotationUnits::deg, false);
+  Feed2.rotateFor(-800, vex::rotationUnits::deg, false);
+  LiftMotorL1.rotateFor(-25, vex::rotationUnits::deg, false);
+  LiftMotorR1.rotateFor(25, vex::rotationUnits::deg, false);
+  LiftMotorL2.rotateFor(-25, vex::rotationUnits::deg, false);
+  LiftMotorR2.rotateFor(25, vex::rotationUnits::deg, true);
+}
 
 #define FIELD_GRAPHIC_WIDTH 250
 #define FIELD_GRAPHIC_HEIGHT 250
@@ -72,21 +82,23 @@ typedef struct {
 // The (0,0) origin of the screen is top left
 #define NUM_AUTO 5
 hotspotType autoHotspots[NUM_AUTO] = {
-  {"   None   ", (FIELD_GRAPHIC_HEIGHT / 2) - 25, (FIELD_GRAPHIC_WIDTH / 2) - 25, 50, 50},
-  {" Red Big  ", 0, 0, 50, 50},
-  {"Red Small ", 0, FIELD_GRAPHIC_WIDTH - 50, 50, 50},
-  {" Blue Big ", FIELD_GRAPHIC_HEIGHT - 50, 0, 50, 50},
-  {"Blue Small", FIELD_GRAPHIC_HEIGHT - 50, FIELD_GRAPHIC_WIDTH - 50, 50, 50},
+    {"   None   ", (FIELD_GRAPHIC_HEIGHT / 2) - 25,
+     (FIELD_GRAPHIC_WIDTH / 2) - 25, 50, 50},
+    {" Red Big  ", 0, 0, 50, 50},
+    {"Red Small ", 0, FIELD_GRAPHIC_WIDTH - 50, 50, 50},
+    {" Blue Big ", FIELD_GRAPHIC_HEIGHT - 50, 0, 50, 50},
+    {"Blue Small", FIELD_GRAPHIC_HEIGHT - 50, FIELD_GRAPHIC_WIDTH - 50, 50, 50},
 };
 
 // Which autonomous was selected (defaults to "none")
 int autoSelect = 0;
 
 // Button pressed (or not) math
-bool isPressed(int idx, int X, int Y)
-{
-  if ((X >= autoHotspots[idx].x) && (X <= autoHotspots[idx].x + autoHotspots[idx].width) &&
-      (Y >= autoHotspots[idx].y) && (Y <= autoHotspots[idx].y + autoHotspots[idx].width)) {
+bool isPressed(int idx, int X, int Y) {
+  if ((X >= autoHotspots[idx].x) &&
+      (X <= autoHotspots[idx].x + autoHotspots[idx].width) &&
+      (Y >= autoHotspots[idx].y) &&
+      (Y <= autoHotspots[idx].y + autoHotspots[idx].width)) {
     return true;
   } else {
     return false;
@@ -110,9 +122,11 @@ void pre_auton(void) {
   // Example: clearing encoders, setting servo positions, ...
 
   // "Accept" button
-  Brain.Screen.drawCircle(55, FIELD_LOC_Y + (FIELD_GRAPHIC_HEIGHT / 2), 50, color::green);
+  Brain.Screen.drawCircle(55, FIELD_LOC_Y + (FIELD_GRAPHIC_HEIGHT / 2), 50,
+                          color::green);
 
-  // Set the screen origin to where we want the field grapic. Makes the math easier.
+  // Set the screen origin to where we want the field grapic. Makes the math
+  // easier.
   Brain.Screen.setOrigin(FIELD_LOC_X, FIELD_LOC_Y);
 
   Brain.Screen.drawImageFromBuffer(field_250_png, 0, 0, field_250_png_len);
@@ -147,9 +161,10 @@ void pre_auton(void) {
 
   // Redraw with only the selected routine
   Brain.Screen.drawImageFromBuffer(field_250_png, 0, 0, field_250_png_len);
-  Brain.Screen.drawRectangle(autoHotspots[autoSelect].x, autoHotspots[autoSelect].y,
-                              autoHotspots[autoSelect].width, autoHotspots[autoSelect].height,
-                              color::transparent);
+  Brain.Screen.drawRectangle(
+      autoHotspots[autoSelect].x, autoHotspots[autoSelect].y,
+      autoHotspots[autoSelect].width, autoHotspots[autoSelect].height,
+      color::transparent);
 
   Brain.Screen.setOrigin(0, 0);
   // Clear the temporary label
@@ -158,28 +173,6 @@ void pre_auton(void) {
   // Print the selected autonomous on the button
   Brain.Screen.setCursor(2, 1);
   Brain.Screen.print(autoHotspots[autoSelect].label);
-
-  Pinch1.setVelocity(pinchspeed, vex::velocityUnits::pct);
-  Pinch2.setVelocity(pinchspeed, vex::velocityUnits::pct);
-  Pinch1.rotateFor(80, vex::rotationUnits::deg, false);
-  Pinch2.rotateFor(-80, vex::rotationUnits::deg, true);
-  Pinch1.resetRotation();
-  Pinch2.resetRotation();
-}
-
-
-void R1() {
-  Pinch1.setVelocity(pinchspeed, vex::velocityUnits::pct);
-  Pinch2.setVelocity(pinchspeed, vex::velocityUnits::pct);
-  Pinch1.rotateFor(50, vex::rotationUnits::deg, false);
-  Pinch2.rotateFor(-50, vex::rotationUnits::deg, true);
-}
-
-void R2() {
-  Pinch1.setVelocity(pinchspeed, vex::velocityUnits::pct);
-  Pinch2.setVelocity(pinchspeed, vex::velocityUnits::pct);
-  Pinch1.rotateFor(-50, vex::rotationUnits::deg, false);
-  Pinch2.rotateFor(50, vex::rotationUnits::deg, true);
 }
 
 void go(double inch) {
@@ -215,18 +208,72 @@ void turn(int deg, std::string lr) {
 void autonomous(void) {
 
   if (autoSelect == 1) {
-    // pick up preload, grab two blocks (orange), and set another orange in tower
+
+    // pick up preload, grab two blocks (orange), and set another orange in
+    // tower first
+    go(20);
+    turn(90, "Left");
+    LiftMotorL1.rotateFor(1275, vex::rotationUnits::deg, false);
+    LiftMotorR1.rotateFor(-1275, vex::rotationUnits::deg, false);
+    LiftMotorL2.rotateFor(1275, vex::rotationUnits::deg, false);
+    LiftMotorR2.rotateFor(-1275, vex::rotationUnits::deg, true);
+    go(12);
+    pulsedown();
+    // go(-12);
+    // LiftMotorL1.rotateFor(1475, vex::rotationUnits::deg, false);
+    // LiftMotorR1.rotateFor(-1475, vex::rotationUnits::deg, false);
+    // LiftMotorL2.rotateFor(1475, vex::rotationUnits::deg, false);
+    // LiftMotorR2.rotateFor(-1475, vex::rotationUnits::deg, true);
   }
   if (autoSelect == 2) {
     // pick up preload, grab one block (orange), and set another orange in tower
+    go(20);
+    turn(90, "Right");
+    LiftMotorL1.rotateFor(1275, vex::rotationUnits::deg, false);
+    LiftMotorR1.rotateFor(-1275, vex::rotationUnits::deg, false);
+    LiftMotorL2.rotateFor(1275, vex::rotationUnits::deg, false);
+    LiftMotorR2.rotateFor(-1275, vex::rotationUnits::deg, true);
+    go(12);
+    pulsedown();
+    // go(-12);
+    // LiftMotorL1.rotateFor(1475, vex::rotationUnits::deg, false);
+    // LiftMotorR1.rotateFor(-1475, vex::rotationUnits::deg, false);
+    // LiftMotorL2.rotateFor(1475, vex::rotationUnits::deg, false);
+    // LiftMotorR2.rotateFor(-1475, vex::rotationUnits::deg, true);
   }
   if (autoSelect == 3) {
     // pick up preload, grab two blocks (green), and set another green in tower
+    // first
+    go(20);
+    turn(90, "Left");
+    LiftMotorL1.rotateFor(-1275, vex::rotationUnits::deg, false);
+    LiftMotorR1.rotateFor(1275, vex::rotationUnits::deg, false);
+    LiftMotorL2.rotateFor(-1275, vex::rotationUnits::deg, false);
+    LiftMotorR2.rotateFor(1275, vex::rotationUnits::deg, true);
+    go(12);
+    pulsedown();
+    // go(-12);
+    // LiftMotorL1.rotateFor(1475, vex::rotationUnits::deg, false);
+    // LiftMotorR1.rotateFor(-1475, vex::rotationUnits::deg, false);
+    // LiftMotorL2.rotateFor(1475, vex::rotationUnits::deg, false);
+    // LiftMotorR2.rotateFor(-1475, vex::rotationUnits::deg, true);
   }
   if (autoSelect == 4) {
     // pick up preload, grab one block (green), and set another green in tower
+    go(20);
+    turn(90, "Right");
+    LiftMotorL1.rotateFor(-1275, vex::rotationUnits::deg, false);
+    LiftMotorR1.rotateFor(1275, vex::rotationUnits::deg, false);
+    LiftMotorL2.rotateFor(-1275, vex::rotationUnits::deg, false);
+    LiftMotorR2.rotateFor(1275, vex::rotationUnits::deg, true);
+    go(12);
+    pulsedown();
+    // go(-12);
+    // LiftMotorL1.rotateFor(1475, vex::rotationUnits::deg, false);
+    // LiftMotorR1.rotateFor(-1475, vex::rotationUnits::deg, false);
+    // LiftMotorL2.rotateFor(1475, vex::rotationUnits::deg, false);
+    // LiftMotorR2.rotateFor(-1475, vex::rotationUnits::deg, true);
   }
-
 }
 
 void usercontrol(void) {
@@ -234,53 +281,84 @@ void usercontrol(void) {
   bool buttondown;
   bool lastbuttonup = false;
   bool lastbuttondown = false;
-  vex::controller().ButtonR1.pressed(R1);
-  vex::controller().ButtonR2.pressed(R2);
+  bool buttonpickup;
+  bool buttonsetdown;
+  bool buttonstop;
 
   while (1) {
     buttonup = vex::controller().ButtonL1.pressing();
     buttondown = vex::controller().ButtonL2.pressing();
-    // buttonopen = vex::controller().ButtonL2.pressing();
-    // buttonclose = vex::controller().ButtonR2.pressing();
-    Pinch1.setVelocity(50, vex::velocityUnits::pct);
-    Pinch2.setVelocity(50, vex::velocityUnits::pct);
+    buttonpickup = vex::controller().ButtonR1.pressing();
+    buttonsetdown = vex::controller().ButtonR2.pressing();
+    buttonstop = vex::controller().ButtonA.pressing();
 
     LeftMotor.spin(vex::directionType::fwd,
-                   vex::controller().Axis3.position() / 1.5,
+                   vex::controller().Axis3.position() / 1.2,
                    vex::velocityUnits::pct);
     RightMotor.spin(vex::directionType::rev,
-                    vex::controller().Axis2.position() / 1.5,
+                    vex::controller().Axis2.position() / 1.2,
                     vex::velocityUnits::pct);
     if ((buttonup == true) && (lastbuttonup == false)) {
-      LiftMotorL.spin(vex::directionType::rev, liftspeed,
+      LiftMotorL1.spin(vex::directionType::rev, liftspeed,
                       vex::velocityUnits::pct);
-      LiftMotorR.spin(vex::directionType::fwd, liftspeed,
+      LiftMotorR1.spin(vex::directionType::fwd, liftspeed,
+                      vex::velocityUnits::pct);
+      LiftMotorL2.spin(vex::directionType::rev, liftspeed,
+                      vex::velocityUnits::pct);
+      LiftMotorR2.spin(vex::directionType::fwd, liftspeed,
                       vex::velocityUnits::pct);
     } else if ((buttondown == true) && (lastbuttondown == false)) {
-      LiftMotorL.spin(vex::directionType::fwd, liftspeed,
+      LiftMotorL1.spin(vex::directionType::fwd, liftspeed,
                       vex::velocityUnits::pct);
-      LiftMotorR.spin(vex::directionType::rev, liftspeed,
+      LiftMotorR1.spin(vex::directionType::rev, liftspeed,
+                      vex::velocityUnits::pct);
+      LiftMotorL2.spin(vex::directionType::fwd, liftspeed,
+                      vex::velocityUnits::pct);
+      LiftMotorR2.spin(vex::directionType::rev, liftspeed,
                       vex::velocityUnits::pct);
     } else if ((buttonup == false) && (buttondown == false)) {
-      // LiftMotorL.spin(vex::directionType::rev, .5, vex::velocityUnits::pct);
-      // LiftMotorR.spin(vex::directionType::fwd, .5, vex::velocityUnits::pct);
-      LiftMotorL.stop(brake);
-      LiftMotorR.stop(brake);
+      LiftMotorL1.spin(vex::directionType::rev, .5, vex::velocityUnits::pct);
+      LiftMotorR1.spin(vex::directionType::fwd, .5, vex::velocityUnits::pct);
+      LiftMotorL2.spin(vex::directionType::rev, .5, vex::velocityUnits::pct);
+      LiftMotorR2.spin(vex::directionType::fwd, .5, vex::velocityUnits::pct);
+      // LiftMotorL1.stop(brake);
+      // LiftMotorR1.stop(brake);
+      // LiftMotorL2.stop(brake);
+      // LiftMotorR2.stop(brake);
+
+    }
+    vex::controller().ButtonB.pressed(pulsedown);
+    if (buttonpickup == true) {
+      Feed1.spin(vex::directionType::rev, feedspeed, vex::velocityUnits::pct);
+      Feed2.spin(vex::directionType::fwd, feedspeed, vex::velocityUnits::pct);
+    }
+    if (buttonsetdown == true) {
+      Feed1.spin(vex::directionType::fwd, feedspeed / 2, vex::velocityUnits::pct);
+      Feed2.spin(vex::directionType::rev, feedspeed / 2, vex::velocityUnits::pct);
+    }
+    if (buttonstop == true) {
+      Feed1.stop(brake);
+      Feed2.stop(brake);
+    }
+    if (vex::controller().ButtonB.pressing()) {
+      buttonpickup = false;
+      buttonsetdown = false;
+      buttonstop = false;
     }
     // if ((buttonopen == true) && (lastbuttonopen == false)) {
-    //   Pinch1.spin(vex::directionType::fwd, pinchspeed, vex::velocityUnits::pct);
-    //   Pinch2.spin(vex::directionType::rev, pinchspeed, vex::velocityUnits::pct);
+    //   Pinch1.spin(vex::directionType::fwd, pinchspeed,
+    //   vex::velocityUnits::pct); Pinch2.spin(vex::directionType::rev,
+    //   pinchspeed, vex::velocityUnits::pct);
     // } else if ((buttonclose == true) && (lastbuttonclose == false)) {
-    //   Pinch1.spin(vex::directionType::rev, pinchspeed, vex::velocityUnits::pct);
-    //   Pinch2.spin(vex::directionType::fwd, pinchspeed, vex::velocityUnits::pct);
+    //   Pinch1.spin(vex::directionType::rev, pinchspeed,
+    //   vex::velocityUnits::pct); Pinch2.spin(vex::directionType::fwd,
+    //   pinchspeed, vex::velocityUnits::pct);
     // } else if ((buttonopen == false) && (buttonclose == false)) {
     //   Pinch1.stop(brake);
     //   Pinch2.stop(brake);
     // }
     buttonup = lastbuttonup;
     buttondown = lastbuttondown;
-    // buttonopen = lastbuttonopen;
-    // buttonclose = lastbuttonclose;
   }
   // Gives the processor time to not burn battery out. NEVER DELETE
   this_thread::sleep_for(20);
